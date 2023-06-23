cb17b6cbd2a8f67ac91b669d3fd21507b88e8a8f
nochnoibriz@gmail.com <nochnoibriz@gmail.com>
2023-06-23 10:21:02 +0600

    Добавьте файлы проекта.

---
+++ Data/UserDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationForTest.Data
+{
+    public class UserDBContext : IdentityDbContext
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
+        {
+        }
+    }
+}
